4033860649e06bc3ad947de7595e487987e007c6
Chris Phillips <lostcolony@gmail.com>
2023-10-17 21:26:15 -0400

    Adding updated mix.exs

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule Jobchecker.MixProject do
       {:json, "~> 1.4"},
       {:elixpath, "~> 0.1.0"},
       {:memento, "~> 0.3.2"},
-      {:gen_smtp, "~> 1.2"}
+      {:gen_smtp, "~> 1.2"},
+      {:rustler, ">= 0.0.0", optional: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
